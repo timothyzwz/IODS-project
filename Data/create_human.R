f755020ec3dee7a4e4055be5dfd184a0eb9a08e0
timothyzwz <33382119+timothyzwz@users.noreply.github.com>
2017-12-03 19:49:03 +0200

    CP5 Data wrangling

--- Data/create_human.R
+++ Data/create_human.R
@@ -1,6 +1,7 @@
 ## Wenzhong Zhang
 ## 28th November 2017
-## Data wrangling exercise for next week's data
+## Updated 03rd December 2017
+## Data wrangling exercise for Human data
 
 # access the dplyr, tidyr and ggplot2 library
 library(dplyr)
@@ -23,7 +24,7 @@ summary(gii)
 
 # change the column names
 colnames(hd) <- c("hdir", "cty", "hdi", "lifebirth", "edutime", "meanedu", "gni", "gnirhdir")
-colnames(gii) <- c("giir", "cty", "gii", "mortalityM", "teenbirth", "ParM", "edu2F", "edu2M", "labF", "labM")
+colnames(gii) <- c("giir", "cty", "gii", "mortalityM", "teenbirth", "ParF", "edu2F", "edu2M", "labF", "labM")
 
 # check the change
 summary(hd)
@@ -48,7 +49,44 @@ str(human)
 dim(human)
 glimpse(human)
 
+# columns to keep
+keep <- c("cty", "eduRatio", "labRatio", "lifebirth", "edutime", "gni", "mortalityM", "teenbirth", "ParF")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+# explore the structure and dimensions of the data
+str(human)
+dim(human)
+glimpse(human)
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human) == TRUE)
+
+# look at the last 10 observations of human
+tail(human_, 10)
+
+# choose everything until the last 7 observations
+human_ <- human_[1:155, ]
+
+# add countries as rownames
+rownames(human_) <- human_$cty
+
+# remove the Country variable
+human_ <- dplyr::select(human_, -cty)
+
+# explore the structure and dimensions of the data
+str(human_)
+dim(human_)
+glimpse(human_)
+
 # Saving the analysis daatset, name it as human.txt, under the data folder
-write.table(human, file = "data/human.txt")
+write.table(human_, file = "Data/human.txt")
 
 
